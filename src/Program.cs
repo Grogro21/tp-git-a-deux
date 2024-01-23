d538a9c86f0758fc273be51a7b6fa9824866af07
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-01-23 16:28:34 +0100

    people container fonctionnel

--- src/Program.cs
+++ src/Program.cs
@@ -7,5 +7,7 @@ Console.WriteLine("saisir prenom");
 string prenom=Console.ReadLine() ;
 src.Person myPerson= new src.Person(nom,prenom);
 Console.WriteLine(nom+" "+prenom);
+List<Person>  peoples = new List<Person>();
+peoples.Add(myPerson);
+src.PeopleContainer myContainer = new PeopleContainer(peoples);
 
-src.PeopleContainer myContainer = { myPerson };
