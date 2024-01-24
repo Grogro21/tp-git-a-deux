f77e3e4a5c31850f0c6bba7f644ed7c10ad98c33
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-01-24 11:08:28 +0100

    ajout répétition du formulaire

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using src;
+using System.ComponentModel.DataAnnotations.Schema;
 
-Console.WriteLine("saisir nom");
-string nom=Console.ReadLine() ;
-Console.WriteLine("saisir prenom");
-string prenom=Console.ReadLine() ;
-src.Person myPerson= new src.Person(nom,prenom);
-Console.WriteLine(nom+" "+prenom);
-List<Person>  peoples = new List<Person>();
-peoples.Add(myPerson);
-src.PeopleContainer myContainer = new PeopleContainer(peoples);
+do
+{
+    Console.WriteLine("saisir nom ou saisisez la lettre q pour quitter le programe :");
+    string nom = Console.ReadLine();
+    Console.WriteLine("Veuilliez ajouter un prénom");
+    string prenom = Console.ReadLine();
+    src.Person myPerson = new src.Person(nom, prenom);
+    Console.WriteLine(nom + " " + prenom);
+    List<Person> peoples = new List<Person>();
+    peoples.Add(myPerson);
+    src.PeopleContainer myContainer = new PeopleContainer(peoples);
+
+} while (nom != "q");
 
