7e26b260ba4ce45c0e0c5682895146bd650378e8
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-01-24 11:48:54 +0100

    ajout de la ligne pour l'ajout des personne

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using src;
 using System.ComponentModel.DataAnnotations.Schema;
+List<Person> peoples = new List<Person>();
 
 do
 {
@@ -10,9 +11,9 @@ do
     string prenom = Console.ReadLine();
     src.Person myPerson = new src.Person(nom, prenom);
     Console.WriteLine(nom + " " + prenom);
-    List<Person> peoples = new List<Person>();
     peoples.Add(myPerson);
     src.PeopleContainer myContainer = new PeopleContainer(peoples);
+    myContainer.People.Add(myPerson);
+} while (string nom != "q");
 
-} while (nom != "q");
-
+List<Person> peoples = new List<Person>();
