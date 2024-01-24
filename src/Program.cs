38bce613a7e9ab6de6304049286b0e504cd88a6e
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-01-24 15:12:05 +0100

    bug fix final?

--- src/Program.cs
+++ src/Program.cs
@@ -5,32 +5,46 @@ using System.ComponentModel.DataAnnotations.Schema;
 List<Person> peoples = new List<Person>();
 string nom;
 string prenom;
+bool isHere;
 src.PeopleContainer myContainer = new PeopleContainer(peoples);
 do
 {
     Console.WriteLine("saisir nom ou saisissez la lettre q pour quitter le programme :");
     nom = Console.ReadLine();
-    if(!nom.SequenceEqual("q"))
-        { 
+    if (!nom.SequenceEqual("q"))
+    {
         Console.WriteLine("Veuillez ajouter un prénom");
         prenom = Console.ReadLine();
         src.Person myPerson = new src.Person(nom, prenom);
         Console.WriteLine(nom + " " + prenom);
-        if (myContainer.People==null)
+        if (myContainer.People == null)
         {
-            myContainer.People= new List<Person>();
-            myContainer.People.Add(myPerson); 
+            myContainer.People = new List<Person>();
+            myContainer.People.Add(myPerson);
         }
-        else if (!myContainer.alreadyContains(myPerson))
-          {
-          myContainer.People.Add(myPerson);
-          }
+
+        else
+        {
+            isHere = false;
+            foreach (Person p in myContainer.People)
+            {
+                if (myPerson.Nom.SequenceEqual(p.Nom) || myPerson.Nom.SequenceEqual(p.Prenom))
+                {
+                    isHere = true;
+                }
+            }
+            if (!isHere)
+            {
+                myContainer.People.Add(myPerson);
+            }
         }
+    }
 
 } 
 while (!nom.SequenceEqual("q"));
+
 if (myContainer.People.Count > 0)
-{
+    {
     myContainer.People = myContainer.SortByFirstName();
     src.SerializePeopleContainerToJson toConvert1 = new SerializePeopleContainerToJson(myContainer);
     string JsonFile = toConvert1.SerializeMethod();
@@ -39,4 +53,4 @@ if (myContainer.People.Count > 0)
     Console.WriteLine(File.ReadAllText(filename));
     
 
-}
+    }
