3a3f358c70214dfc0802255d2b21265a5319021f
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-01-24 12:25:03 +0100

    correction

--- src/Program.cs
+++ src/Program.cs
@@ -3,30 +3,27 @@ using System.Text.Json;
 using src;
 using System.ComponentModel.DataAnnotations.Schema;
 List<Person> peoples = new List<Person>();
-
-
+string nom;
+string prenom;
+src.PeopleContainer myContainer = new PeopleContainer(peoples);
 do
 {
-    Console.WriteLine("saisir nom ou saisisez la lettre q pour quitter le programe :");
-    string nom = Console.ReadLine();
-    Console.WriteLine("Veuilliez ajouter un prénom");
-    string prenom = Console.ReadLine();
+    Console.WriteLine("saisir nom ou saisissez la lettre q pour quitter le programme :");
+    nom = Console.ReadLine();
+    Console.WriteLine("Veuillez ajouter un prénom");
+    prenom = Console.ReadLine();
     src.Person myPerson = new src.Person(nom, prenom);
     Console.WriteLine(nom + " " + prenom);
-    peoples.Add(myPerson);
-    src.PeopleContainer myContainer = new PeopleContainer(peoples);
-    myContainer.People.Add(myPerson);
     if (!myContainer.alreadyContains(myPerson))
 {
     myContainer.People.Add(myPerson);
 }
 
-} while (string nom != "q");
+} while (!nom.SequenceEqual("q"));
 src.PeopleContainer listeTri = myContainer.SortByFirstName();
 src.SerializePeopleContainerToJson toConvert = new SerializePeopleContainerToJson(myContainer);
 string JsonFile = toConvert.SerializeMethod();
 string filename = "savefile.json";
 File.WriteAllText(filename, JsonFile);
 Console.WriteLine(File.ReadAllText(filename));
-List<Person> peoples = new List<Person>();
 
