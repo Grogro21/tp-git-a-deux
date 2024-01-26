cec8199a69e2a520dc4e7e498d9439bfc6cf9b92
Grogro21 <74548874+Grogro21@users.noreply.github.com>
2024-01-26 10:48:35 +0100

    commentaires

--- src/Program.cs
+++ src/Program.cs
@@ -17,7 +17,7 @@ do
         prenom = Console.ReadLine();
         src.Person myPerson = new src.Person(nom, prenom);
         Console.WriteLine(nom + " " + prenom);
-        if (myContainer.People == null)
+        if (myContainer.People == null) //si c'est la première personne, rien à comparer, on l'ajoute au container
         {
             myContainer.People = new List<Person>();
             myContainer.People.Add(myPerson);
@@ -26,14 +26,14 @@ do
         else
         {
             isHere = false;
-            foreach (Person p in myContainer.People)
+            foreach (Person p in myContainer.People)  //On parcourt la liste de personnes 
             {
-                if (myPerson.Nom.SequenceEqual(p.Nom) || myPerson.Nom.SequenceEqual(p.Prenom))
+                if (myPerson.Nom.SequenceEqual(p.Nom) || myPerson.Nom.SequenceEqual(p.Prenom)) //Si elle y est déja, on ne l'ajoute pas
                 {
                     isHere = true;
                 }
             }
-            if (!isHere)
+            if (!isHere) //Si elle n'y est pas, c'est bon on ajoute
             {
                 myContainer.People.Add(myPerson);
             }
@@ -42,12 +42,12 @@ do
 } 
 while (!nom.SequenceEqual("q"));
 
-if (myContainer.People != null)
+if (myContainer.People != null) //obligé de vérifier que le container n'est pas vide sinon ça va crash
     {
-    myContainer.People = myContainer.SortByFirstName();
+    myContainer.People = myContainer.SortByFirstName(); //tri par nom
     src.SerializePeopleContainerToJson toConvert1 = new SerializePeopleContainerToJson(myContainer);
-    string JsonFile = toConvert1.SerializeMethod();
+    string JsonFile = toConvert1.SerializeMethod(); 
     string filename = "savefile.json";
-    File.WriteAllText(filename, JsonFile);
+    File.WriteAllText(filename, JsonFile); //crée le fichier savefile.json et écrit dedans
     Console.WriteLine(File.ReadAllText(filename));
     }
